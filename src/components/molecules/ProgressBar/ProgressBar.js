46d24ba3b6abd4f58c21222604fad364da1b7733
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-12-14 13:27:30 +0900

    finished progress-bar

---
+++ src/components/molecules/ProgressBar/ProgressBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Box from "../../atoms/Box/Box";
+import "./ProgressBar.css";
+
+export const ProgressBar = ({ total, current }) => {
+  console.log(current);
+  return (
+    <Box style={{ position: "relative", width: "125px" }}>
+      <hr
+        className="current-progress"
+        style={
+          current !== 0
+            ? { width: `calc(100% * ${current}/${total})` }
+            : { width: "0", border: "0px" }
+        }
+      />
+      <hr className="total-progress" style={{ width: "100%" }} />
+    </Box>
+  );
+};
+
+export default ProgressBar;
