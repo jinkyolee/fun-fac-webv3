c451ede17351d17aacb971da7abab27089c167d8
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-12-28 16:02:29 +0900

    min changes

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { community } from "../assets/images/0_images";
 import { HeaderInstance as Header } from "../components/0_Instances/HeaderInstance";
 import Image from "../components/atoms/Image/Image";
 import StandardPage from "../components/templates/StandardPage";
@@ -9,17 +8,7 @@ export const Community = () => {
     <StandardPage
       className="justify-center"
       header={<Header />}
-      body={
-        <Image
-          src={community}
-          style={{
-            width: "80%",
-            objectFit: "contain",
-            marginTop: "100px",
-            marginBottom: "100px",
-          }}
-        />
-      }
+      body={<h1>Hello</h1>}
     />
   );
 };
