ff59d0b15214dd872f0c9639d2cf94ea11a2588c
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2021-01-14 12:36:09 +0900

    profile description mishap fix

--- src/constants/profile.js
+++ src/constants/profile.js
@@ -7,10 +7,10 @@ import {
 } from "assets/images/Team/0_teamImgExp";
 
 const profileProps = [
-  { name: "손서연", imageSrc: berProfile, job: "웹 UX, UI 디자인" },
+  { name: "손서연", imageSrc: berProfile, job: "게임 시나리오 / UI 디자인" },
   { name: "손영웅", imageSrc: herProfile, job: "iOS 개발자" },
   { name: "최서연", imageSrc: sukProfile, job: "iOS 개발자" },
-  { name: "김해린", imageSrc: melProfile, job: "웹 UX, UI 디자인" },
+  { name: "김해린", imageSrc: melProfile, job: "웹 콘텐츠 제작 / UI 디자인" },
   { name: "이진교", imageSrc: hobProfile, job: "웹 개발자" },
 ];
 
