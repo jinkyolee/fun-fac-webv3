cea0ad303abf20e1036c113967c105ea1d34dd1c
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2021-03-07 22:20:08 +0900

    header configured for mobile

--- src/components/0_Instances/HeaderInstance.js
+++ src/components/0_Instances/HeaderInstance.js
@@ -10,7 +10,7 @@ import { authService } from "fbaseInst/fbase";
 import { logo } from "assets/images/0_images";
 import "./HeaderInstance.css";
 import getDeviceWidth from "functions/getDeviceWidth";
-import ListIcon from "assets/svg/Community/ListIcon";
+import { ListIconBlue as ListIcon } from "assets/svg/Community/ListIcon";
 
 export const HeaderInstance = () => {
   const [language, setLanguage] = useRecoilState(languageState);
@@ -89,7 +89,11 @@ export const HeaderInstance = () => {
             />
           </Box>,
         ]}
-        secondCol={tabs}
+        secondCol={tabs.map(({ url, label }, index) => {
+          return (
+            <LinkedButton to={url} label={label} className="tab" key={index} />
+          );
+        })}
         thirdCol={
           toggled && (
             <Box className="last-wrap">
@@ -114,13 +118,84 @@ export const HeaderInstance = () => {
     return (
       <HeaderMobile
         firstCol={
-          <Button
-            className="toggle-menuM"
-            label={<ListIcon />}
-            onClick={toggleHeader}
-          />
+          <>
+            <Button
+              className="toggle-menuM"
+              label={<ListIcon />}
+              onClick={toggleHeader}
+            />
+            {toggled && (
+              <Button
+                className="lang-btn mobile gotham"
+                onClick={(e) => {
+                  e.preventDefault();
+                  let newLang;
+
+                  if (language === "kr") {
+                    newLang = "en";
+                  } else {
+                    newLang = "kr";
+                  }
+
+                  window.localStorage.setItem("languageSetting", newLang);
+                  setLanguage(newLang);
+                }}
+                label="한 / En"
+                key={0}
+              />
+            )}
+          </>
         }
-        dropdown={toggled && <Box className="vertical-flex"></Box>}
+        dropdown={
+          toggled && (
+            <Box className="vertical-flex" style={{ alignItems: "center" }}>
+              {tabs.map(({ url, label }, index) => {
+                return (
+                  <LinkedButton
+                    to={url}
+                    label={label}
+                    className="tab mobile"
+                    key={index}
+                  />
+                );
+              })}
+              <Box className="vertical-flex centered tab-box">
+                <LinkedButton
+                  to="/game"
+                  label="GAME"
+                  key={0}
+                  className="tab mobile gotham"
+                />
+                <LinkedButton
+                  to="/team"
+                  label="ABOUT US"
+                  key={1}
+                  className="tab mobile gotham"
+                />
+              </Box>
+              <Box
+                className="vertical-flex centered"
+                style={{ position: "fixed", bottom: "2.75rem" }}
+              >
+                {loggedIn ? (
+                  <Button
+                    className="home-auth-btn mobile"
+                    key="8"
+                    label={languageState === "kr" ? "로그아웃" : "LOGOUT"}
+                    onClick={() => authService.signOut()}
+                  />
+                ) : (
+                  <LinkedButton
+                    className="home-auth-btn mobile"
+                    to="/login"
+                    label={languageState === "kr" ? "로그인" : "LOGIN"}
+                  />
+                )}
+                <LinkedImage src={logo} className="logo mobile" to="/" />
+              </Box>
+            </Box>
+          )
+        }
       />
     );
   }
--- src/recoil/selectors.js
+++ src/recoil/selectors.js
@@ -1,6 +1,5 @@
 import { selector } from "recoil";
 import { languageState, postsCollection } from "./atoms";
-import { LinkedButton } from "components/atoms/Button/Button";
 
 export const fontState = selector({
   key: "fontState",
@@ -24,30 +23,26 @@ export const headerTabs = selector({
     const language = get(languageState);
 
     return [
-      <LinkedButton
-        to="/timeline"
-        label={language === "kr" ? "5대궁과 함께한 시간" : "Timeline"}
-        key="0"
-        className="tab"
-      />,
-      <LinkedButton
-        to="/test"
-        label={language === "kr" ? "궁 추천 테스트" : "Test"}
-        key="1"
-        className="tab"
-      />,
-      <LinkedButton
-        to="/persons"
-        label={language === "kr" ? "인물 포커스" : "Person"}
-        key="2"
-        className="tab"
-      />,
-      <LinkedButton
-        to="/community"
-        label={language === "kr" ? "커뮤니티" : "Forum"}
-        key="3"
-        className="tab"
-      />,
+      {
+        url: "/timeline",
+        label: language === "kr" ? "5대궁과 함께한 시간" : "Timeline",
+      },
+      {
+        url: "/test",
+        label: language === "kr" ? "궁 추천 테스트" : "Test",
+      },
+      {
+        url: "/test",
+        label: language === "kr" ? "궁 추천 테스트" : "Test",
+      },
+      {
+        url: "/persons",
+        label: language === "kr" ? "인물 포커스" : "Person",
+      },
+      {
+        url: "/community",
+        label: language === "kr" ? "커뮤니티" : "Forum",
+      },
     ];
   },
 });
