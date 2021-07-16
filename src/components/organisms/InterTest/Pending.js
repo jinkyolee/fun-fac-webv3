08140a6bb860a060e506ddb96a77476a3f0ee87a
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2021-07-16 14:58:28 +0900

    edit Todo & eng ver.GBTI progress

--- src/components/organisms/InterTest/Pending.js
+++ src/components/organisms/InterTest/Pending.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Box } from "components/atoms/Box/Box";
 import { ClickableImage, Image } from "components/atoms/Image/Image";
 import copyLink from "functions/local/Community/copyLink";
-import Text from "components/atoms/Text/Text";
+import {Text} from "components/atoms/Text/Text";
 import "./Pending.css";
 import { pendingText } from "constants/textConsts/test";
+import Button from "components/atoms/Button/Button";
 
 export const Pending = ({ setTestState, language }) => {
   const { text, logo, illust, share, start } = pendingText(language);
@@ -14,14 +15,13 @@ export const Pending = ({ setTestState, language }) => {
       className="vertical-flex align-center"
       style={{ width: "100%", position: "relative" }}
     >
-      <Image src={logo} className="main-test-logo" />
+      <Text children={logo} className="main-test-logo yangjin" />
       <Image src={illust} className="main-illust" />
       <Text className="main-subtext yangjin">{text}</Text>
-      <ClickableImage
-        src={start}
+      <Button
+        label={start}
         className="start-button"
         onClick={() => setTestState("started")}
-        alt="테스트 시작하기"
       />
       <ClickableImage
         src={share}
--- src/constants/textConsts/test.js
+++ src/constants/textConsts/test.js
@@ -14,22 +14,22 @@ export const pendingText = (language) => {
         나와 찰떡인 궁은?
       </>
     );
-    logo = testLogo;
+    logo = "궁비티아이";
     illust = testIllust;
     share = testShare;
-    start = testStart;
+    start = "테스트 시작";
   } else {
     text = (
       <>
-        Which of the five
+        Find out the best match between
         <br />
-        Great Palaces fits you
+        you and palaces!
       </>
     );
-    logo = testLogo;
+    logo = "GBTI";
     illust = testIllust;
     share = testShare;
-    start = testStart;
+    start = "test start";
   }
   return { text, logo, illust, share, start };
 };
@@ -61,7 +61,7 @@ export const inProgressText = (language, setResult, increasePoints, points) => {
           },
         },
         {
-          choiceText: ["감성가득 파리의 거리"],
+          choiceText: ["럭셔리한 5성급 호텔"],
           increaseFunc: () => {
             increasePoints(["bok", "gyeong"]);
           },
@@ -136,75 +136,75 @@ export const inProgressText = (language, setResult, increasePoints, points) => {
   } else {
     return {
       0: [
-        { question: "요즘 나는 말이야" },
+        { question: "I've been feeling" },
         {
-          choiceText: ["지루하고 따분..", "새로운 자극이 필요해!"],
+          choiceText: ["bored lately,", "I need a new excitement!"],
           increaseFunc: () => {
             increasePoints(["duk", "soo"]);
           },
         },
         {
-          choiceText: ["몸도 마음도 지쳤어", "힐링이 필요해!"],
+          choiceText: ["exhausted lately,", "I need relaxation!"],
           increaseFunc: () => {
             increasePoints(["bok", "hui"]);
           },
         },
       ],
       1: [
-        { question: "해외 여행에서 내가 머물고 싶은 숙소는?" },
+        { question: "When I’m traveling, I’d like to stay at" },
         {
-          choiceText: ["현지 느낌 충만한 에어비*비"],
+          choiceText: ["airb*b, full of the local vibe "],
           increaseFunc: () => {
             increasePoints(["gyeong", "duk"]);
           },
         },
         {
-          choiceText: ["감성가득 파리의 거리"],
+          choiceText: ["luxurious five-star hotel"],
           increaseFunc: () => {
             increasePoints(["bok", "gyeong"]);
           },
         },
       ],
       2: [
-        { question: "나의 최애 카페 음료는?" },
+        { question: "My go-cafe drink is?" },
         {
-          choiceText: ["클래식은 영원하다!", "아메리카노"],
+          choiceText: ["Americano!", "classic never dies"],
           increaseFunc: () => {
             increasePoints(["duk", "gyeong"]);
           },
         },
         {
-          choiceText: ["단 게 최고!", "과일 스무디"],
+          choiceText: ["Fruit smoothie.", "sweet is the best!"],
           increaseFunc: () => {
             increasePoints(["gyeong", "hui"]);
           },
         },
       ],
       3: [
-        { question: "떠날 수만 있다면, 내가 당장 가고 싶은 여행지는?" },
+        { question: "If I could leave right now, which city do you want to go?" },
         {
-          choiceText: ["다채로움의 도시 뉴욕"],
+          choiceText: ["City of diversity, New York"],
           increaseFunc: () => {
             increasePoints(["hui", "duk"]);
           },
         },
         {
-          choiceText: ["감성가득 파리의 거리"],
+          choiceText: ["Romantic Paris"],
           increaseFunc: () => {
             increasePoints(["bok", "duk"]);
           },
         },
       ],
       4: [
-        { question: "내가 좋아하는 영화는?" },
+        { question: "My favorite movie genre is" },
         {
-          choiceText: ["타이타닉 , 어바웃타임같은", "드라마&로맨스 장르"],
+          choiceText: ["drama & romance", "e.g. Titanic, About time"],
           increaseFunc: () => {
             increasePoints(["gyeong", "bok"]);
           },
         },
         {
-          choiceText: ["인셉션, 매트릭스같은 SF 장르 "],
+          choiceText: ["science fiction", "e.g. Inception, Matrix"],
           increaseFunc: () => {
             increasePoints(["gyeong", "hui"]);
           },
@@ -213,17 +213,17 @@ export const inProgressText = (language, setResult, increasePoints, points) => {
       5: [
         {
           question:
-            "문화 생활에 쓸 수 있는 3만원짜리 티켓이 생겼다. 나의 선택은?",
+            "You have a free ticket for cultural life. What is Your choice? ",
         },
         {
-          choiceText: ["오랜만에 친구들과 놀이공원"],
+          choiceText: ["go amusement park with friends"],
           increaseFunc: () => {
             increasePoints(["duk", "soo"]);
             setResult(points.current);
           },
         },
         {
-          choiceText: ["조용히 사색하며 즐기는 전시회"],
+          choiceText: ["an exhibition with", "quiet ontemplation"],
           increaseFunc: () => {
             increasePoints(["soo", "gyeong"]);
             setResult(points.current);
